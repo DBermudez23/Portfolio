0c7b2eefc5ab56c2cd5c9f8cbab0cf4180d37253
Daniel <danielbermudezwuw@gmail.com>
2025-04-24 23:06:52 -0500

    Adding project

--- src/projectsInfo.js
+++ src/projectsInfo.js
@@ -6,6 +6,7 @@ import mLearning from "./assets/mLearning.jpeg";
 import threeInLine from "./assets/threeInLine.jpeg";
 import javaAirplane from "./assets/javaAirplane.jpeg";
 import Datastructure from "./assets/DataStructure.jpeg";
+import visorImagenes from "./assets/visorImagenes.png";
 
 const infoProjects = [
     {
@@ -16,13 +17,6 @@ const infoProjects = [
         github : 'https://github.com/DBermudez23/News-App'
     },
     {
-        name : 'MEMORY GAME (SOLO HTML Y CSS)',
-        description : 'Juego de memoria con cartas, practica pura de estilizado y HTML. Realizado con HTML y CSS sin una sola linea de algún lenguaje de programación.',
-        image : memoryGame,
-        url : 'https://memoty-game-db.netlify.app/',
-        github : 'https://github.com/DBermudez23/memory-game'
-    },
-    {
         name : 'APP DE TAREAS (CRUD)',
         description : 'Aplicación web para guardar y eliminar tareas. Realizado con django y bootstrap.',
         image : taskList,
@@ -30,17 +24,17 @@ const infoProjects = [
         github : 'https://github.com/DBermudez23/Tasks-app-CRUD'
     },
     {
+        name : 'VISOR Y EDITOR DE IMAGENES CON PyQt & NumPy',
+        description : 'Desarrollada en Python con PyQt6 que permite visualizar y editar imágenes con técnicas de procesamiento con NumPy y Matplotlib.',
+        image : visorImagenes,
+        url : 'https://github.com/DBermudez23/Visor_imagenes',
+        github : 'https://github.com/DBermudez23/Visor_imagenes'
+    },
+    {
         name : 'JAVA APLICACIÓN SISTEMA DE RESERVAS AEROPUERTO',
         description : 'Aplicación Java desarrollada con Maven y Swing para la gestión de reservas de vuelos en una aerolínea.',
         image : javaAirplane,
         github : 'https://github.com/DBermudez23/Aeropuerto'
-    },
-    {
-        name : 'APP CINEMA',
-        description : 'Aplicación web para buscar películas, series de televisión y actores. Utiliza la API de The Movie Database (TMDb).',
-        image : cinemaApp,
-        url : 'https://cinema-appdb-tmdbapi.netlify.app/',
-        github : 'https://github.com/DBermudez23/cinema-app-TMDB-API'
     },
     {
         name : 'CLASIFICACIÓN DE PAISES POR RIESGO ENERGETICO (MACHINE LEARNING)',
@@ -53,6 +47,20 @@ const infoProjects = [
         description : 'Un proyecto de C++ centrado en la implementación y manipulación de estructuras de datos básicas, como pilas, colas y árboles.',
         image : Datastructure,
         github : 'https://github.com/DBermudez23/Data-Structure-Project'
+    },
+    {
+        name : 'MEMORY GAME (SOLO HTML Y CSS)',
+        description : 'Juego de memoria con cartas, practica pura de estilizado y HTML. Realizado con HTML y CSS sin una sola linea de algún lenguaje de programación.',
+        image : memoryGame,
+        url : 'https://memoty-game-db.netlify.app/',
+        github : 'https://github.com/DBermudez23/memory-game'
+    },
+    {
+        name : 'APP CINEMA',
+        description : 'Aplicación web para buscar películas, series de televisión y actores. Utiliza la API de The Movie Database (TMDb).',
+        image : cinemaApp,
+        url : 'https://cinema-appdb-tmdbapi.netlify.app/',
+        github : 'https://github.com/DBermudez23/cinema-app-TMDB-API'
     },
     {
         name : '3 EN LINEA',
